1a9c6ca35c8a73ca5c89cabff3503a5cde463da6
Saurav <sp.gharti@gmail.com>
2018-10-05 23:58:05 +0545

    add migration for models

--- django_scaffold/wsgi.py
+++ django_scaffold/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djago_scaffold.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_scaffold.settings")
 
 application = get_wsgi_application()
