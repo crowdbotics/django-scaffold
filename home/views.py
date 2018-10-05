166f60da8372a20d606d31c3c4805f9c4cbab19e
Saurav <sp.gharti@gmail.com>
2018-10-06 00:10:25 +0545

    add packages

--- home/views.py
+++ home/views.py
@@ -7,7 +7,9 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
-	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
+        {'name': 'djangorestframework', 'url': 'http://pypi.python.org/pypi/djangorestframework/3.7.7'},
+        {'name': 'django-bootstrap4', 'url': 'http://pypi.python.org/pypi/django-bootstrap4/0.0.5'},
+        {'name': 'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
     ]
     context = {
         'customtext': CustomText.objects.first(),
