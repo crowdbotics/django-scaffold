e07f0dba38761038f4dae78d8299fea9ffb55620
Dan DeFelippi <dan@driverdan.com>
2019-07-02 16:23:25 -0400

    Update user settings and validation, return user on signin

--- {{cookiecutter.project_slug}}/home/api/v1/viewsets.py
+++ {{cookiecutter.project_slug}}/home/api/v1/viewsets.py
@@ -1,10 +1,11 @@
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
-from home.api.v1.serializers import SignupSerializer, CustomTextSerializer, HomePageSerializer
+from home.api.v1.serializers import SignupSerializer, CustomTextSerializer, HomePageSerializer, UserSerializer
 from home.models import CustomText, HomePage
 
 
@@ -14,10 +15,19 @@ class SignupViewSet(ModelViewSet):
 
 
 class LoginViewSet(ViewSet):
+    """Based on rest_framework.authtoken.views.ObtainAuthToken"""
     serializer_class = AuthTokenSerializer
 
     def create(self, request):
-        return ObtainAuthToken().post(request)
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        user_serializer = UserSerializer(user)
+        return Response({'token': token.key, 'user': user_serializer.data})
 
 
 class CustomTextViewSet(ModelViewSet):
