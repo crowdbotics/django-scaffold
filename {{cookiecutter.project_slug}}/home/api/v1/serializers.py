91b38a1a1ad3a093b60e020aa95d525ba52d6750
Dan DeFelippi <dan@driverdan.com>
2019-07-12 23:10:49 -0400

    Call setup_user_email to add email for allauth

--- {{cookiecutter.project_slug}}/home/api/v1/serializers.py
+++ {{cookiecutter.project_slug}}/home/api/v1/serializers.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
-from allauth.utils import generate_unique_username
+from django.http import HttpRequest
+from django.utils.translation import ugettext_lazy as _
+from allauth.account import app_settings as allauth_settings
+from allauth.utils import email_address_exists, generate_unique_username
 from allauth.account.adapter import get_adapter
+from allauth.account.utils import setup_user_email
 from rest_framework import serializers
 
 from home.models import CustomText, HomePage
@@ -25,6 +29,12 @@ class SignupSerializer(serializers.ModelSerializer):
             }
         }
 
+    def _get_request(self):
+        request = self.context.get('request')
+        if request and not isinstance(request, HttpRequest) and hasattr(request, '_request'):
+            request = request._request
+        return request
+
     def validate_email(self, email):
         email = get_adapter().clean_email(email)
         if allauth_settings.UNIQUE_EMAIL:
@@ -45,6 +55,8 @@ class SignupSerializer(serializers.ModelSerializer):
         )
         user.set_password(validated_data.get('password'))
         user.save()
+        request = self._get_request()
+        setup_user_email(request, user, [])
         return user
 
     def save(self, request=None):
