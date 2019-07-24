8079aa92580ebc5e2fdc44aff788d32a2aeb4555
Dan DeFelippi <dan@driverdan.com>
2019-07-24 16:13:05 +0000

    Mobile push notifications (#9)
    * Add is_mobile to cookiecutter.json
    
    * Update Pipfile with pinned versions and conditional fcm-django, remove Pipfile.lock
    
    * Add fcm_django to apps and config
    
    * Fix incorrect comparisons

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from allauth.account.views import confirm_email
 
 urlpatterns = [
     path('', include('home.urls')),
