43af45a2f336550492dd04ba761784ff4ecdead7
Daniel Sousa <daniel.s@crowdbotics.com>
2019-10-03 21:54:50 +0100

    Add swagger api docs to the scaffold (#25)
    * Feature: Swagger API Docs
    
    - Installs drf_yasg
    - Adds /api-docs url to view swaggerui
    
    * Add packaging to Pipfile
    
    `drf_yasg` needs `packaging` as per open issue:
    https://github.com/axnsan12/drf-yasg/issues/412
    
    * Remove commented config

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
@@ -61,6 +61,7 @@ THIRD_PARTY_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'django_extensions',
+    'drf_yasg',
 {% if cookiecutter.is_mobile == "y" %}
     # start fcm_django push notifications
     'fcm_django',
