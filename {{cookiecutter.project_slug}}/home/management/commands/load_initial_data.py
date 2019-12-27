be76869364c73fbf9899b99f9bb1bf9feb1d6853
Dan DeFelippi <dan@driverdan.com>
2019-12-27 10:53:28 -0600

    Use double quotes to allow apostrophes in project_name

--- {{cookiecutter.project_slug}}/home/management/commands/load_initial_data.py
+++ {{cookiecutter.project_slug}}/home/management/commands/load_initial_data.py
@@ -10,14 +10,14 @@ def load_initial_data():
             This is the sample application created and deployed from the crowdbotics slack app. You can
             view list of packages selected for this application below
         </p>"""
-    customtext_title = '{{cookiecutter.project_name}}'
+    customtext_title = "{{cookiecutter.project_name}}"
     CustomText.objects.create(title=customtext_title)
     HomePage.objects.create(body=homepage_body)
 
 
 class Command(BaseCommand):
     can_import_settings = True
-    help = 'Load initial data to db'
+    help = "Load initial data to db"
 
     def handle(self, *args, **options):
         load_initial_data()
