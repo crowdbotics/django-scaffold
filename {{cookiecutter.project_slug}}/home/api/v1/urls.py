a2c2d82713626bb901c2e87a12b214c314760680
Dan DeFelippi <dan@driverdan.com>
2020-04-17 11:52:24 -0400

    Merge develop to master (#70)
    * Update waitress requirement in /{{cookiecutter.project_slug}}
    
    Updates the requirements on [waitress](https://github.com/Pylons/waitress) to permit the latest version.
    - [Release notes](https://github.com/Pylons/waitress/releases)
    - [Changelog](https://github.com/Pylons/waitress/blob/master/CHANGES.txt)
    - [Commits](https://github.com/Pylons/waitress/compare/v1.4.2...v1.4.3)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * PLAT-555 - Replace api/v1/report implementation (#57)
    
    * Remove api/v1/report view
    
    * Remove special CrowdboticsExclusive api permission
    
    * Remove api/v1/report view
    
    * Revert "Remove api/v1/report view"
    
    This reverts commit 8eee3d5fa3575c5808a5ba6f415add9ce206b10b.
    
    * Remove api/v1/report url and import
    
    * Update django-extensions requirement in /{{cookiecutter.project_slug}}
    
    Updates the requirements on [django-extensions](https://github.com/django-extensions/django-extensions) to permit the latest version.
    - [Release notes](https://github.com/django-extensions/django-extensions/releases)
    - [Changelog](https://github.com/django-extensions/django-extensions/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/django-extensions/django-extensions/compare/2.2.6...2.2.8)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Update drf-yasg requirement in /{{cookiecutter.project_slug}} (#63)
    
    Updates the requirements on [drf-yasg](https://github.com/axnsan12/drf-yasg) to permit the latest version.
    - [Release notes](https://github.com/axnsan12/drf-yasg/releases)
    - [Changelog](https://github.com/axnsan12/drf-yasg/blob/master/docs/changelog.rst)
    - [Commits](https://github.com/axnsan12/drf-yasg/compare/1.17.0...1.17.1)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * PLAT-1921: Update Docker configuration and instructions (#65)
    
    Some users experienced the limitations of the Alpine platform. Therefore,
    it makes sense to switch to an LTS Ubuntu, which is easier to maintain, too.
    
    - Postgres files are mounted to project subdirectory
    - Heavy usage of `docker-compose.override.yml`, so it's easier to write
    a deployment configuration.
    - Users will also benefit from an expanded README including the docker
    setup.
    - Pinning of the versions in Docker related files will also ensure fewer
    unwanted surprises in the future.
    
    * Update psycopg2 requirement in /{{cookiecutter.project_slug}} (#68)
    
    Updates the requirements on [psycopg2](https://github.com/psycopg/psycopg2) to permit the latest version.
    - [Release notes](https://github.com/psycopg/psycopg2/releases)
    - [Changelog](https://github.com/psycopg/psycopg2/blob/master/NEWS)
    - [Commits](https://github.com/psycopg/psycopg2/commits)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    
    * Update django-extensions requirement in /{{cookiecutter.project_slug}} (#66)
    
    Updates the requirements on [django-extensions](https://github.com/django-extensions/django-extensions) to permit the latest version.
    - [Release notes](https://github.com/django-extensions/django-extensions/releases)
    - [Changelog](https://github.com/django-extensions/django-extensions/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/django-extensions/django-extensions/compare/2.2.8...2.2.9)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    Co-authored-by: Daniel Sousa <daniel.s@crowdbotics.com>
    Co-authored-by: Dmitrii Kharlamov <d.a.kharlamov@gmail.com>

--- {{cookiecutter.project_slug}}/home/api/v1/urls.py
+++ {{cookiecutter.project_slug}}/home/api/v1/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from home.api.v1.viewsets import SignupViewSet, LoginViewSet, HomePageViewSet, CustomTextViewSet, AppReportView
+from home.api.v1.viewsets import (
+    SignupViewSet,
+    LoginViewSet,
+    HomePageViewSet,
+    CustomTextViewSet,
+)
 
 router = DefaultRouter()
-router.register('signup', SignupViewSet, basename='signup')
-router.register('login', LoginViewSet, basename='login')
-router.register('customtext', CustomTextViewSet)
-router.register('homepage', HomePageViewSet)
+router.register("signup", SignupViewSet, basename="signup")
+router.register("login", LoginViewSet, basename="login")
+router.register("customtext", CustomTextViewSet)
+router.register("homepage", HomePageViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("report", AppReportView.as_view(), name="app_report"),
 ]
