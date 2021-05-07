f1178867e3a5d42f5dad867dc8c6ce188f573728
Daniel <daniel.s@crowdbotics.com>
2021-05-07 19:51:35 +0100

    Cover migrations issues in the try/catch block

--- {{cookiecutter.project_slug}}/modules/urls.py
+++ {{cookiecutter.project_slug}}/modules/urls.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from django.conf import settings
 from django.urls import path, include
+from django.db.utils import ProgrammingError
 
 
 urlpatterns = []
@@ -11,12 +12,12 @@ urlpatterns = []
 
 try:
     modules_dir = f"{settings.BASE_DIR}/modules/"
-    urls = Path(modules_dir).rglob('urls.py')
+    urls = Path(modules_dir).rglob("urls.py")
     for url in urls:
-        module_name, _ = url.as_posix().split('/')[-2:]
+        module_name, _ = url.as_posix().split("/")[-2:]
         if not module_name == "modules":
             urlpatterns += [
                 path(f"{module_name}/", include(f"modules.{module_name}.urls"))  # noqa
             ]
-except (ImportError, IndexError):
+except (ImportError, IndexError, ProgrammingError):
     pass
