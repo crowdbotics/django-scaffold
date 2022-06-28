cc6a72a01799e024903aabc44327a7aa9bbd6d2b
Daniel <daniel.s@crowdbotics.com>
2022-06-28 19:30:56 +0100

    null check all_module_options and module_options

--- {{cookiecutter.project_slug}}/modules/utils.py
+++ {{cookiecutter.project_slug}}/modules/utils.py
@@ -9,9 +9,14 @@ GLOBAL_OPTIONS_FILE_PATH = f"{Path.cwd()}/modules/options.json"
 def get_options(module_slug, option_key):
     with open(GLOBAL_OPTIONS_FILE_PATH, "r") as f:
         all_module_options = json.loads(f.read())
-        all_module_options = all_module_options["module_options"]
+        all_module_options = all_module_options.get("module_options", None)
 
-    option_value = all_module_options[module_slug].get(option_key, False)
+    option_value = None
+
+    if all_module_options:
+        module_options = all_module_options.get(model_slug, None)
+        if module_options:
+            option_value = module_options.get(option_key, None)
 
     default_value = getattr(
         importlib.import_module(f"modules.{module_slug}.options"), option_key
