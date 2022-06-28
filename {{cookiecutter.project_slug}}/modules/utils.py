088f25b0445e5f95989b0b717582b132ad8e365a
Daniel <daniel.s@crowdbotics.com>
2022-06-28 17:30:17 +0100

    Fix get_options dict get call

--- {{cookiecutter.project_slug}}/modules/utils.py
+++ {{cookiecutter.project_slug}}/modules/utils.py
@@ -8,16 +8,13 @@ GLOBAL_OPTIONS_FILE_PATH = f"{Path.cwd()}/modules/options.json"
 
 def get_options(module_slug, option_key):
     with open(GLOBAL_OPTIONS_FILE_PATH, "r") as f:
-        module_options = json.loads(f.read())
+        all_module_options = json.loads(f.read())
+        all_module_options = all_module_options["module_options"]
 
-    option_value = [
-        module.get(option_key)
-        for module in module_options["module_options"]
-        if module.get("slug") == module_slug
-    ]
+    option_value = all_module_options[module_slug].get(option_key, False)
 
     default_value = getattr(
         importlib.import_module(f"modules.{module_slug}.options"), option_key
     )
 
-    return option_value[0] if option_value else default_value
+    return option_value if option_value else default_value
