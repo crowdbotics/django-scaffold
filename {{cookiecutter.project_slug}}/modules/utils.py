ecff5e4334d864e9d21db89e2eeae9b109dc39f9
Daniel Sousa <daniel.s@crowdbotics.com>
2022-06-29 15:21:39 +0100

    Fix get_options dict get call (#396)
    * Fix get_options dict get call
    
    * null check all_module_options and module_options

--- {{cookiecutter.project_slug}}/modules/utils.py
+++ {{cookiecutter.project_slug}}/modules/utils.py
@@ -8,16 +8,18 @@ GLOBAL_OPTIONS_FILE_PATH = f"{Path.cwd()}/modules/options.json"
 
 def get_options(module_slug, option_key):
     with open(GLOBAL_OPTIONS_FILE_PATH, "r") as f:
-        module_options = json.loads(f.read())
+        all_module_options = json.loads(f.read())
+        all_module_options = all_module_options.get("module_options", None)
 
-    option_value = [
-        module.get(option_key)
-        for module in module_options["module_options"]
-        if module.get("slug") == module_slug
-    ]
+    option_value = None
+
+    if all_module_options:
+        module_options = all_module_options.get(model_slug, None)
+        if module_options:
+            option_value = module_options.get(option_key, None)
 
     default_value = getattr(
         importlib.import_module(f"modules.{module_slug}.options"), option_key
     )
 
-    return option_value[0] if option_value else default_value
+    return option_value if option_value else default_value
