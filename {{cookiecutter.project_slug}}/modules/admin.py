5bb916f9baf23f0a48088dd78154f32608daf3fe
Rodrigo <matematica.a3k@gmail.com>
2022-07-13 13:46:38 -0300

    PLAT-7015: Django Modules as Python Packages (#397)
    - Update modules.utils
    - Update modules.admin
    - Update modules.urls
    - Add "pipenv" to Pipfile
    - Update Pipfile.lock
    
    Implementation Details
    ======================
    
    In order to support Django Modules as Python Packages, modules app's
    utils, admin and urls discovery must be generalized to any subdirectory.
    
    This is not a breaking change, as current Django Modules should work.
    
    Co-authored-by: Daniel Sousa <daniel.s@crowdbotics.com>
    
    Co-authored-by: Daniel Sousa <daniel.s@crowdbotics.com>

--- {{cookiecutter.project_slug}}/modules/admin.py
+++ {{cookiecutter.project_slug}}/modules/admin.py
@@ -1,19 +1,16 @@
 from importlib import import_module
 from pathlib import Path
 
-from django.conf import settings
+from .utils import posixpath_to_modulepath
 
 # BE CAREFUL! Do not remove or change this code snippet, this is needed to get
 # Crowdbotics' official modules working properly.
 
 try:
-    modules_dir = f"{settings.BASE_DIR}/modules/"
-    admins = list(Path(modules_dir).rglob('admin.py'))
+    admins = Path(".").rglob('admin.py')
 
     for admin in admins:
-        module_name, _ = admin.as_posix().split('/')[-2:]
-        if not module_name == "modules":
-            module = import_module(f"modules.{module_name}.admin")
-            from module import *  # noqa
+        module = import_module(posixpath_to_modulepath(admin))
+        from module import *  # noqa
 except (ImportError, IndexError):
     pass
--- {{cookiecutter.project_slug}}/modules/utils.py
+++ {{cookiecutter.project_slug}}/modules/utils.py
@@ -6,6 +6,11 @@ from pathlib import Path
 GLOBAL_OPTIONS_FILE_PATH = f"{Path.cwd()}/modules/options.json"
 
 
+def posixpath_to_modulepath(posixpath):
+    module_parent_path = posixpath.parent.as_posix().replace("/", ".")
+    return f"{module_parent_path}.{posixpath.stem}"
+
+
 def get_options(module_slug, option_key):
     with open(GLOBAL_OPTIONS_FILE_PATH, "r") as f:
         all_module_options = json.loads(f.read())
@@ -14,12 +19,17 @@ def get_options(module_slug, option_key):
     option_value = None
 
     if all_module_options:
-        module_options = all_module_options.get(model_slug, None)
+        module_options = all_module_options.get(module_slug, None)
         if module_options:
             option_value = module_options.get(option_key, None)
 
+    module_options_file = next(
+        Path(".").rglob(f"**/{module_slug.replace('-', '_')}/**/options.py")
+    )
+
+    options_module = posixpath_to_modulepath(module_options_file)
     default_value = getattr(
-        importlib.import_module(f"modules.{module_slug}.options"), option_key
+        importlib.import_module(options_module), option_key
     )
 
     return option_value if option_value else default_value
